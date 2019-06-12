9ba88f778fe7605758d97872f4cf4f6bc03a8257
Alex <aleksgrin96@gmail.com>
2019-06-12 14:44:15 +0300

    Исправляет замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var COMENTS_VARIANTS = ['Всё отлично!',
+var COMMENTS = ['Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
@@ -8,8 +8,15 @@ var COMENTS_VARIANTS = ['Всё отлично!',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 var NAMES = ['Саша', 'Петя', 'Даша', 'Ростислав', 'Августина', 'Любомир', 'Евдокия', 'Стеша', 'fox777', 'Настя'];
-var OBJECTS_AMOUNT = 25;
-function generateRandom(min, max) {
+var AMOUNT = 25;
+
+var pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+var picturesElement = document.querySelector('.pictures');
+var fragment = document.createDocumentFragment();
+
+function getRandomIntegerFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -17,20 +24,20 @@ function generateObjects() {
   var photos = [];
   photos[i] = {
     url: 'photos/' + (i + 1) + '.jpg',
-    likes: generateRandom(15, 200),
-    comments: generateComments()
+    likes: getRandomIntegerFromInterval(15, 200),
+    comments: generateComments(getRandomIntegerFromInterval(1, 5))
   };
 
   return photos;
 }
 
-function generateComments() {
+function generateComments(commentsQuantity) {
   var comments = [];
-  for (var i = 0; i < generateRandom(1, 5); i++) {
+  for (var i = 0; i < commentsQuantity; i++) {
     comments[i] = {
-      avatar: 'img/avatar-' + generateRandom(1, 6) + '.svg',
-      message: COMENTS_VARIANTS[generateRandom(0, COMENTS_VARIANTS.length - 1)],
-      name: NAMES[generateRandom(0, NAMES.length)]
+      avatar: 'img/avatar-' + getRandomIntegerFromInterval(1, 6) + '.svg',
+      message: COMMENTS[getRandomIntegerFromInterval(0, COMMENTS.length - 1)],
+      name: NAMES[getRandomIntegerFromInterval(0, NAMES.length)]
     };
   }
 
@@ -46,18 +53,8 @@ function createPicturesDOM(image) {
   return picture;
 }
 
-function putElementIntoBlock(whatToPut, whereToPut) {
-  whereToPut.appendChild(createPicturesDOM(whatToPut));
+for (var i = 0; i < AMOUNT; i++) {
+  fragment.appendChild(createPicturesDOM(generateObjects()));
 }
 
-var pictureTemplate = document.querySelector('#picture')
-  .content
-  .querySelector('.picture');
-var pictures = document.querySelector('.pictures');
-var fragment = document.createDocumentFragment();
-
-for (var i = 0; i < OBJECTS_AMOUNT; i++) {
-  putElementIntoBlock(generateObjects(i), fragment);
-}
-
-pictures.appendChild(fragment);
+picturesElement.appendChild(fragment);
