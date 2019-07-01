e030d28739b45f3406633c1df95f300ae55e210a
Alex <aleksgrin96@gmail.com>
2019-07-02 00:24:58 +0300

    Рефакторинг

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict';
 (function () {
-  var GELLERY_PHOTO_COUNT = 25;
-  var uploadInputElement = document.querySelector('#upload-file');
+  var GALLERY_PHOTO_COUNT = 25;
 
-  var photos = window.data.get(GELLERY_PHOTO_COUNT);
+  var uploadInputElement = document.querySelector('#upload-file');
+  var photos = window.data.get(GALLERY_PHOTO_COUNT);
+
   uploadInputElement.addEventListener('change', window.popupSetup.open);
 
   window.gallery.render(photos);
