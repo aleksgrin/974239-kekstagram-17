547225fb3fcd14f3f9326460fddc9ef0fc8a31d5
Alex <aleksgrin96@gmail.com>
2019-07-07 13:53:31 +0300

    Добавляет обработчик события при клике на фото

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 (function () {
   var uploadInputElement = document.querySelector('#upload-file');
+  var picturesElement = document.querySelector('.pictures');
   var photos = [];
 
   function loadHandler(data) {
@@ -8,11 +9,19 @@
     window.gallery.render(photos);
     window.filter.show();
     window.filter.init(photos);
-    window.bigPicture.show();
-    window.bigPicture.render(photos[0]);
 
-    for (var i = 0; i < photos[0].comments.length; i++) {
-      window.bigPicture.insert(photos[0].comments[i]);
+    picturesElement.addEventListener('click', onPhotoClick);
+
+    function onPhotoClick(evt) {
+      var vr;
+      vr = photos.find(function (element) {
+        return evt.target.src.endsWith(element.url);
+      });
+      window.bigPicture.show();
+      window.bigPicture.render(vr);
+      for (var i = 0; i < vr.comments.length; i++) {
+        window.bigPicture.insert(vr.comments[i]);
+      }
     }
   }
 
